429c56fbf55fab13243fc6ff0617a81434bfb493
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-04-07 21:17:54 +0530

    feat: check for calculator in App

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Calculator from "./calculator";
 import "./App.css";
 
 const App = () => {
-  return <div className="app-container"></div>;
+  return (
+    <div className="app-container">
+      <Calculator />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/component/calculator/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Calculator = () => {
+  const initialState = {
+    displayValue: "0",
+    numbers: [],
+    operators: [],
+    selectedOperator: "",
+    storedValue: "",
+  };
+  const [state, setState] = useState(initialState);
+
+  const callOperator = () => {};
+
+  const setOperator = () => {};
+
+  const updateDisplay = () => {};
+
+  return <div></div>;
+};
+
+export default Calculator;
